8439f05bd80dc0b73a7caadaadde0d423605a208
Максим Филин <inet.soul@gmail.com>
2024-07-05 09:53:46 +0300

    переделка вывода массива в task1

--- task1/index.php
+++ task1/index.php
@@ -1,49 +1,43 @@
 <?php
 
-$data = [
-	['Иванов', 'Математика', 5],
-	['Иванов', 'Математика', 4],
-	['Иванов', 'Математика', 5],
-	['Петров', 'Математика', 5],
-	['Сидоров', 'Физика', 4],
-	['Иванов', 'Физика', 4],
-	['Петров', 'ОБЖ', 4],
-];
+    $data = [
+        ['Иванов', 'Математика', 5],
+        ['Иванов', 'Математика', 4],
+        ['Иванов', 'Математика', 5],
+        ['Петров', 'Математика', 5],
+        ['Сидоров', 'Физика', 4],
+        ['Иванов', 'Физика', 4],
+        ['Петров', 'ОБЖ', 4],
+        ['Петров', 'Физика', 5],
+    ];
 
-// Сортировка и суммирование данных
-$result = array_reduce($data, function ($carry, $item) {
-    $name = $item[0];
-    $subject = $item[1];
-    $score = $item[2];
+    // Сортировка и суммирование данных
+    $result = array_reduce($data, function ($carry, $item) {
+        $name = $item[0];
+        $subject = $item[1];
+        $score = $item[2];
 
-    if (!isset($carry[$name])) {
-        $carry[$name] = [];
+        if (!isset($carry[$name])) {
+            $carry[$name] = [];
+        }
+
+        if (!isset($carry[$name][$subject])) {
+            $carry[$name][$subject] = 0;
+        }
+
+        $carry[$name][$subject] += $score;
+
+        return $carry;
+    }, []);
+
+    // Получение списка предметов
+    $subjects = [];
+    foreach ($result as $name => $scores) {
+        foreach ($scores as $k => $v) {
+            if (!in_array($k, $subjects))
+                $subjects[] = $k;
+        }
     }
-
-    if (!isset($carry[$name][$subject])) {
-        $carry[$name][$subject] = 0;
-    }
-
-    $carry[$name][$subject] += $score;
-
-    return $carry;
-}, []);
-
-$subjects = [];
-foreach ($result as $name => $scores) {
-    foreach ($scores as $k => $v) {
-        if (!in_array($k, $subjects))
-            $subjects[] = $k;
-    }
-}
-
-    echo '<pre>';
-    print_r($result);
-    echo '</pre>';
-
-    echo '<pre>';
-    print_r($subjects);
-    echo '</pre>';
 ?>
 
 <!DOCTYPE html>
@@ -66,26 +60,14 @@ foreach ($result as $name => $scores) {
         echo "</tr>";
 
         foreach ($result as $name => $subjectScore) {
-            //ksort($subjects);
             echo "<tr><td>$name</td>";
-
-            print_r($subjectScore);
-
-            $i = 0;
             foreach ($subjects as $subject){
-                $j = 0;
-                foreach ($subjectScore as $k=>$v){
-                    if($k == $subject && $i == $j)
-                        echo "<td>$v</td>";
-                    else{
-                        if($i == $j)
-                            echo "<td></td>";
-                    }
-                    $j ++;
-                }
-                $i++;
+                if(isset($subjectScore[$subject]))
+                    echo "<td>$subjectScore[$subject]</td>";
+                else
+                    echo "<td></td>";
             }
-            
+
             echo "</tr>";
         }
     ?>
